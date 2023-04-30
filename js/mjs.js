d34c21294757b7edd7cd7b5a6c3ef0789a9574a4
John Carlo <johncarlolabanero@gmail.com>
2023-05-01 06:14:12 +0800

    stadardize development

--- js/mjs.js
+++
@@ -1,8 +0,0 @@
-function hideDiv(element){
-    var sibling = element.nextElementSibling; 
-    if(sibling.style.display == "none"){
-        sibling.style.display = "block";
-    } else {
-        sibling.style.display = "none";
-    }
-}
